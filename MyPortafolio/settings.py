12d22a8103b093919ebe6fe47fb1f283c35ef764
Carlos David <carlos.monier87@gmail.com>
2022-12-07 22:20:06 -0600

    Final

--- MyPortafolio/settings.py
+++ MyPortafolio/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'MyPortafolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portafolio',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'das45,b3*',
+        'PORT': '5432',
     }
 }
 
--- portafolio/urls.py
+++ portafolio/urls.py
@@ -15,21 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import views
+from portafolio import views
 
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('contacto/', views.contacto, name="contacto"),
-    path('portafolio/', include('portafolio.urls')),
-    path('admin/', admin.site.urls),
+    
+    path('portafolio/', views.portafolio, name="portafolio"),
+    
     
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     
 
